1295370fb55d6c905e3af6bdac0d34da548d0245
yiminghe <yiminghe@gmail.com>
2025-09-24 22:09:16 +0800

    new bug

---
+++ middleware.ts
@@ -0,0 +1,10 @@
+
+
+export default function middleware() {
+  throw new Error('thrown from middleware');
+  // return new Response('Hello, world!')
+}
+
+export const config = {
+  runtime: 'nodejs',
+};
